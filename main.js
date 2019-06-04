980f88cf3621772756287fbed3e854b651cab4e9
Lais Lima <lais@cosmobots.io>
2019-06-04 14:55:28 -0300

    função que ordena

--- main.js
+++ main.js
@@ -15,18 +15,34 @@ const insertRows = bunda => {
   return table
 }
 
+
+
+const orderList = (livroA, livroB, propriedade) => {
+  if(livroA[propriedade].toLowerCase() < livroB[propriedade].toLowerCase()){
+    return -1
+  }
+  if(livroA[propriedade].toLowerCase() > livroB[propriedade].toLowerCase()){
+    return 1
+  }
+  return 0
+}
+
 const ordenarLivro = document.getElementById("ordenar-livro")
+const ordenarAutor = document.getElementById("ordenar-autor")
+const ordenarGenero = document.getElementById("ordenar-genero")
 
 ordenarLivro.addEventListener("click", (e) => {
-  const listaOrdenada = listaDeLivrosJs.livros.sort((livroA, livroB) => {
-    if(livroA.livro.toLowerCase() < livroB.livro.toLowerCase()){
-      return -1
-    }
-    if(livroA.livro.toLowerCase() > livroB.livro.toLowerCase()){
-      return 1
-    }
-    return 0
-  })
+  const listaOrdenada = listaDeLivrosJs.livros.sort((livroA, livroB) => orderList(livroA, livroB, "livro"))
+  document.getElementById("table-section").innerHTML = insertRows(listaOrdenada)
+})
+
+ordenarAutor.addEventListener("click", (e) => {
+  const listaOrdenada = listaDeLivrosJs.livros.sort((livroA, livroB) => orderList(livroA, livroB, "autor"))
+  document.getElementById("table-section").innerHTML = insertRows(listaOrdenada)
+})
+
+ordenarGenero.addEventListener("click", (e) => {
+  const listaOrdenada = listaDeLivrosJs.livros.sort((livroA, livroB) => orderList(livroA, livroB, "genero"))
   document.getElementById("table-section").innerHTML = insertRows(listaOrdenada)
 })
 
